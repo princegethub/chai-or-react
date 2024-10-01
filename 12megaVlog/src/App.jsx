1350ddec9ac439c0952c9532820cd2f44ffc1645
Prince Mishra <pm618209@gmail.com>
2024-10-02 00:50:43 +0530

    12 Project Enviorment Setup Complete

---
+++ 12megaVlog/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>{import.meta.env.VITE_APPWRITE_URL}</h1>
+    </>
+  )
+}
+
+export default App
---
+++ 12megaVlog/src/conf/Conf.js
@@ -0,0 +1,8 @@
+const conf = {
+  appWriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
+  appWriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+  appWriteDataBaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+  appWriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+  appWriteBucketId: String(import.meta.env.VITE_APPWRITE_BUKET_ID),
+}
+export default conf
