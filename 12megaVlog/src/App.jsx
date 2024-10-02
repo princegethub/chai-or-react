d612fd14e4337f8b27edb28c53e71d367638f7a1
Prince Mishra <pm618209@gmail.com>
2024-10-02 18:15:20 +0530

    Video 21 End

--- 12megaVlog/src/App.jsx
+++ 12megaVlog/src/App.jsx
@@ -1,13 +1,47 @@
-import { useState } from 'react'
-import './App.css'
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import "./App.css";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import {Outlet} from 'react-router-dom'
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
 
-  return (
-    <>
-      <h1>{import.meta.env.VITE_APPWRITE_URL}</h1>
-    </>
-  )
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .catch((error) => {
+        console.log("App.jsx :: Get Account ::  Error", error);
+       
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  return !loading ? (
+    <div className="min-h-screen flex content-between flex-wrap bg-gray-400 ">
+     <div className="w-full block">
+      <Header/>
+      <main>
+        {/* Outlet AAyega  */}
+       Todo:  <Outlet />
+      </main>
+      <Footer/>
+     </div>
+    </div>
+  ) : null;
 }
 
-export default App
+export default App;
--- 12megaVlog/src/appwrite/config.js
+++ 12megaVlog/src/appwrite/config.js
@@ -2,17 +2,120 @@ import { Client, Databases, ID, Storage, Query } from "appwrite";
 import conf from "../conf/conf";
 
 export class Service {
-  client = new Client()
+  client = new Client();
   databases;
+  buket;
 
-  constructor(){
-    this.client 
-    .setEndpoint(conf.appWriteUrl)
-    .setProject(conf.appWriteProjectId)
+  constructor() {
+    this.client
+      .setEndpoint(conf.appWriteUrl)
+      .setProject(conf.appWriteProjectId);
     this.databases = new Databases(this.client);
+    this.buket = new Storage(this.client);
   }
 
+  async createPost({ title, slug, content, featuredImage, status, userId }) {
+    return await this.databases.createDocument(
+      conf.appWriteDataBaseId,
+      conf.appWriteCollectionId,
+      slug,
+      {
+        title,
+        content,
+        featuredImage,
+        status,
+        userId,
+      }
+    );
+  }
 
+  async getPost(slug) {
+    try {
+      return await this.databases.getDocument(
+        conf.appWriteDataBaseId,
+        conf.appWriteCollectionId,
+        slug
+      );
+    } catch (error) {
+      console.error("App Write Service ::  getPost :: error", error);
+      return false;
+    }
+  }
+
+  async getPosts(quries = [Query.equal("status", "active")]) {
+    try {
+      return await this.databases.listDocuments(
+        conf.appWriteDataBaseId,
+        conf.appWriteCollectionId,
+        quries
+      );
+    } catch (error) {
+      console.error("App Write Service ::  allPost :: error", error);
+      return false;
+    }
+  }
+
+  async updatePost(slug, { title, content, featuredImage, status }) {
+    return await this.databases.updateDocument(
+      conf.appWriteDataBaseId,
+      conf.appWriteCollectionId,
+      slug,
+      {
+        title,
+        content,
+        slug,
+        featuredImage,
+        status,
+      }
+    );
+  }
+
+  async deletePost() {
+    try {
+      await this.databases.deleteDocument(
+        conf.appWriteDataBaseId,
+        conf.appWriteCollectionId,
+        slug
+      );
+      return true;
+    } catch (error) {
+      console.log("App Write service ::  deletePost :: error", error);
+      return false;
+    }
+  }
+
+  // file upload service
+
+  async uploadFile(file) {
+    try {
+      return await this.buket.createFile(
+        conf.appWriteBucketId,
+        ID.unique(),
+        file
+      );
+    } catch (error) {
+      console.log("AppWrite Service ::   uploadFile :: error", error);
+      return false;
+    }
+  }
+
+  async deleteFile(fileId) {
+    try {
+      await this.buket.deleteFile(conf.appWriteBucketId, fileId);
+      return true;
+    } catch (error) {
+      console.log("AppWrite Service :: deleteFile :: error", error);
+      return false;
+    }
+  }
+
+  getFilePreview(fileId) {
+    try {
+     return this.buket.getFilePreview(conf.appWriteBucketId, fileId);
+    } catch (error) {
+      console.log("Appwrite Service ::  getFilePreview :: error", error);
+    }
+  }
 }
 
 const service = new Service();
